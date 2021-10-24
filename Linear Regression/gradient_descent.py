989c71b14f004039adbf8bf42f9bdcafed4b40f3
jason-zavala <jzavala2012@gmail.com>
2021-10-23 20:05:53 -0600

    found optimal lr for gradient descent (question 4) L

--- Linear Regression/gradient_descent.py
+++ Linear Regression/gradient_descent.py
@@ -1,4 +1,4 @@
-import math, os
+import math, os, decimal
 
 
 def dot(a, b):
@@ -15,14 +15,15 @@ def subtract(a, b):
         return res
 
 def magnitude(a):
-        squared_sum = 0
+        squared_sum = decimal.Decimal(0)
         for val in a:
+                val = decimal.Decimal(val)
                 squared_sum += val ** 2
         return math.sqrt(squared_sum)
 
 def gradient_descent(data, w, b , lr):
         fsize = len(data[0]) - 1
-        max_steps = 100
+        max_steps = 10000
 
         for step in range(max_steps):
                 gradient = [0] * fsize
@@ -38,19 +39,25 @@ def gradient_descent(data, w, b , lr):
                         b_slope += -(y - wx -b)
                 
                 ss = [lr * g for g in gradient]
+                prev_weight = w 
                 w = subtract(w, ss)
                 b -= b_slope * lr
-                print(w, b)
+                #print(w, b)
 
-                if magnitude(ss) < lr:
-                        print(step)
-                        break
+                weight_delta = subtract(prev_weight, w)
+                threshold = 10e-6
+
+                if magnitude(weight_delta) < threshold:
+                        return step
+                
+        return step
+
        
 def read_data(csv):
         data = []
         with open(csv, 'r') as f:
                 for line in f:
-                        val = list(map(int, line.strip().split(',')))
+                        val = list(map(float, line.strip().split(',')))
                         data.append(val)
         f.close()
         return data
@@ -60,13 +67,17 @@ def main():
         train_file = os.path.join("concrete","train.csv")
         test_file = os.path.join("concrete","test.csv")
 
-        data = read_data(sample_file)
+        data = read_data(train_file)
 
-        w = [-1, 1, -1]
-        b = -1
-        lr = 0.01
-
-        gradient_descent(data, w, b, lr)
+        w = [0] * 7
+        b = 0
+        lr = 1
+        r = 9999
+        while r == 9999:
+                print(lr)
+                r = gradient_descent(data, w, b, lr)
+                lr = lr/2
+        print(r)
 
 if __name__ == "__main__":
     main()
