918b6b4f6f2f620ffb735ead90646d0a63e8d278
jason-zavala <jzavala2012@gmail.com>
2021-11-02 18:27:36 -0600

    update run.sh

--- Perceptron/run.sh
+++ Perceptron/run.sh
@@ -1,3 +1,5 @@
-python3 .\Perceptron.py standard
-python3 .\Perceptron.py average
-python3 .\Perceptron.py voted
+#!/bin/sh
+
+python3 Perceptron.py standard
+python3 Perceptron.py average
+python3 Perceptron.py voted
