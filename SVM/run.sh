9be7f09182e5903946591215065f005f85ae3d45
jason-zavala <jzavala2012@gmail.com>
2021-11-18 14:54:36 -0700

    formatting:

--- SVM/run.sh
+++ SVM/run.sh
@@ -3,7 +3,8 @@ echo " "
 echo "Jason Zavala proudly presents:"
 echo "HW4"
 echo "Section 2"
-echo "part 2a"
+echo " "
+echo "PART 2A"
 echo " "
 
 echo "C = 100/873"
@@ -14,7 +15,8 @@ echo "C = 700/873"
 python3 svm.py .802 0
 
 echo " "
-echo "part 2b"
+echo "###########################################################"
+echo "PART 2b"
 echo " "
 echo "C = 100/873"
 python3 svm.py 0.114 1
@@ -22,7 +24,7 @@ echo "C = 500/873"
 python3 svm.py 0.573 1
 echo "C = 700/873"
 python3 svm.py .802 1
-echo " "
 
-echo "HW4 Section 2 q2 part 3"
 echo " "
+echo "###########################################################"
+echo "PART 3"
