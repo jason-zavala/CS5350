f28b1a1cd6e4186a2bf719b7e0e222418dca1e2c
jason-zavala <jzavala2012@gmail.com>
2021-11-18 13:15:48 -0700

    formatting

--- SVM/run.sh
+++ SVM/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-echo "Jason Zavala proudly presents: \n"
-echo "HW4 /n"
-echo "HW4 Section 2 q2 part 2 \n"
+echo " "
+echo "Jason Zavala proudly presents:"
+echo "HW4"
+echo "Section 2"
+echo "part 2"
 python3 svm.py 0.114
 python3 svm.py 0.573
 python3 svm.py .802 
