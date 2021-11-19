257ef81024e5b71a5822cdfa6fd6cb327bd1d0b0
jason-zavala <jzavala2012@gmail.com>
2021-11-18 18:18:14 -0700

    formatting

--- SVM/svm.py
+++ SVM/svm.py
@@ -55,6 +55,45 @@ def svm(data, w, a, c, learning_rate, t, schedule):
             rate_learned = learning_rate/(epoch + 1)
         previous_weight = w # keep track so we can compare it to track delta_weight
 
+        #Don’t forget to shuffle the training examples at the start of each epoch 
+        random.shuffle(data)
+
+        for d in copy.deepcopy(data): 
+            w_0 = w[:-1] # w without the bias
+            genuine_or_forged = d[-1] # capturing the last colum, per the data desc this is the genuine or forged value
+            d[-1] = 1 # fold b into x
+            prediction = dot(w, d)  
+            #just check if it's misclassified, and if it is we update the weight_vector
+            if genuine_or_forged * prediction <= 1: 
+                add_left_side = sub(w, scalar_multiplication((w_0 + [0]), rate_learned))
+                add_right_side = scalar_multiplication(d, rate_learned * c * SIZE * genuine_or_forged)
+                w = add(add_left_side, add_right_side)
+            else:
+                w[:-1] = scalar_multiplication(w[:-1], 1 - rate_learned) 
+        # calculate delta weight
+        delta_weight = sub(previous_weight, w)
+        if magnitude(delta_weight) < 10e-3:
+            print("converged at:", epoch)
+            return w
+
+    return w
+
+def dual_svm(data, w, a, c, learning_rate, t, schedule):
+    # swap all 0's in the final colum for -1's 
+    SIZE = len(data)
+    for d in data: 
+        if d[-1] == 0:
+            d[-1] = -1
+
+
+    for epoch in range(t):
+        # update our learning rate, the whole point is that we update over each itr to get it converge
+        if schedule == 0:
+            rate_learned = learning_rate / (1 + (learning_rate * epoch / a))
+        else:
+            rate_learned = learning_rate/(epoch + 1)
+        previous_weight = w # keep track so we can compare it to track delta_weight
+
 
         random.shuffle(data)
 
@@ -78,8 +117,9 @@ def svm(data, w, a, c, learning_rate, t, schedule):
 
     return w
 
+
 def get_error(data, learned_weight):
-        # Next pt
+    # Next pt
     error = 0
     #swap out all the 0's for -1's
     for d in data: 
@@ -100,15 +140,17 @@ def get_error(data, learned_weight):
 
 def main():
 
+    # SETUP RUNTIME ARGS
     schedule = 0 if len(sys.argv) <= 2 else float(sys.argv[2])
     c = float(100/873) if len(sys.argv) == 1 else float(sys.argv[1])
+    #################################################################
 
     #collecting our data
     train_file = os.path.join("bank-note","train.csv")
-    test_file = os.path.join("bank-note","test.csv")
+    test_file  = os.path.join("bank-note","test.csv")
 
     data_training = read_file(train_file)
-    data_testing = read_file(test_file)
+    data_testing  = read_file(test_file)
 
     w  = [0] * len(data_training[0]) # initializt an array of length data_training full of just 0's
     lr = 0.0001 # learning rate
