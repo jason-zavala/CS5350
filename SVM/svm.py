724d31efa1af93bfb7197d9991f814ca384f423f
jason-zavala <jzavala2012@gmail.com>
2021-11-20 15:03:40 -0700

    progress on s4ection 2 pt 3

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,4 +1,6 @@
 import os, sys, random, math, decimal, copy
+import numpy as np
+import scipy.optimize as opt
 
 
 def read_file(csv):
@@ -39,6 +41,25 @@ def magnitude(a):
 def scalar_multiplication(vec, s):
     return [s * index for index in vec]
 
+def get_error(data, learned_weight):
+    # Next pt
+    error = 0
+    #swap out all the 0's for -1's
+    for d in data: 
+        if d[-1] == 0:
+            d[-1] = -1
+
+    for d in copy.deepcopy(data): 
+        gen_or_forg = d[-1]
+
+        d[-1] = 1
+
+        prediction = dot(learned_weight, d)
+
+        if gen_or_forg * prediction <= 0:
+            error+=1
+    return error/len(data)
+    
 def svm(data, w, a, c, learning_rate, t, schedule):
     # swap all 0's in the final colum for -1's 
     SIZE = len(data)
@@ -78,71 +99,47 @@ def svm(data, w, a, c, learning_rate, t, schedule):
 
     return w
 
+def quad_convex_equation(alpha, *args):
+    x = args[0]
+    y = args[1]
+
+    xx = (x*x.T)
+    yy = (y*y.T)
+
+    yyaaxx = alpha.T.dot((xx*yy)[0, 0] * alpha)
+    
+    return (0.5) *  yyaaxx - np.sum(alpha)
+
+def constraint(alpha, *args):
+    y = args[1]
+    return np.sum(alpha * y)
+
 def dual_svm(data, w, a, c, learning_rate, t, schedule):
-    # swap all 0's in the final colum for -1's 
+
     SIZE = len(data)
-    for d in data: 
-        if d[-1] == 0:
-            d[-1] = -1
+    x = np.matrix([data[d][:-1] for d in range(SIZE)])
+    y = np.matrix([data[d][-1]  for d in range(SIZE)]).T
+    y = np.where(y == 0, -1, y) # condition, true, else
+    
+    init_guess = np.random.rand(SIZE)
+    args = (x, y)
+    method = "SLSQP"
+    bounds = [(0, c)] * SIZE # we want to define our bounds from 0 -> c for every size
+    constraints = [{'type' : 'eq', 'fun': constraint, 'args':args}]
 
-
-    for epoch in range(t):
-        # update our learning rate, the whole point is that we update over each itr to get it converge
-        if schedule == 0:
-            rate_learned = learning_rate / (1 + (learning_rate * epoch / a))
-        else:
-            rate_learned = learning_rate/(epoch + 1)
-        previous_weight = w # keep track so we can compare it to track delta_weight
-
-
-        random.shuffle(data)
-
-        for d in copy.deepcopy(data): 
-            w_0 = w[:-1] # w without the bias
-            genuine_or_forged = d[-1] # capturing the last colum, per the data desc this is the genuine or forged value
-            d[-1] = 1 # fold b into x
-            prediction = dot(w, d)  
-            #just check if it's misclassified, and if it is we update the weight_vector
-            if genuine_or_forged * prediction <= 1: 
-                add_left_side = sub(w, scalar_multiplication((w_0 + [0]), rate_learned))
-                add_right_side = scalar_multiplication(d, rate_learned * c * SIZE * genuine_or_forged)
-                w = add(add_left_side, add_right_side)
-            else:
-                w[:-1] = scalar_multiplication(w[:-1], 1 - rate_learned) 
-        # calculate delta weight
-        delta_weight = sub(previous_weight, w)
-        if magnitude(delta_weight) < 10e-3:
-            print("converged at:", epoch)
-            return w
-
+    optimal_alpha = opt.minimize(quad_convex_equation, init_guess, args, method, bounds = bounds, constraints = constraints)
+    print(optimal_alpha.x)
     return w
 
 
-def get_error(data, learned_weight):
-    # Next pt
-    error = 0
-    #swap out all the 0's for -1's
-    for d in data: 
-        if d[-1] == 0:
-            d[-1] = -1
-
-    for d in copy.deepcopy(data): 
-        gen_or_forg = d[-1]
-
-        d[-1] = 1
-
-        prediction = dot(learned_weight, d)
-
-        if gen_or_forg * prediction <= 0:
-            error+=1
-    return error/len(data)
-    
 
 def main():
 
     # SETUP RUNTIME ARGS
-    schedule = 0 if len(sys.argv) <= 2 else float(sys.argv[2])
     c = float(100/873) if len(sys.argv) == 1 else float(sys.argv[1])
+    schedule = 0 if len(sys.argv) <= 2 else float(sys.argv[2])
+    domain = "primal" if len(sys.argv) <= 3 else "dual"
+
     #################################################################
 
     #collecting our data
@@ -157,11 +154,22 @@ def main():
     t  = 100 # t, although this is a bit confusing maybe I should name this something more pacific
     a  = 0.0001
 
-    learned_weight = svm(data_training, w, a, c, lr, t, schedule)
-    print("Learned weight vector: ", [round(num, 3) for num in learned_weight])
-    # get error percentage
-    print("Test error    :", get_error(data_testing, learned_weight) )
-    print("Training error:", get_error(data_training, learned_weight), "\n")
+    if domain == "primal":
+        learned_weight = svm(data_training, w, a, c, lr, t, schedule)
+        print("Learned weight vector: ", [round(num, 3) for num in learned_weight])
+        # get error percentage
+        print("Test error    :", get_error(data_testing, learned_weight) )
+        print("Training error:", get_error(data_training, learned_weight), "\n")
+    else:
+        learned_weight = dual_svm(data_training, w, a, c, lr, t, schedule)
+        sys.exit(0)
+    
+
+
+    #################################################################
+    #                       QUESTION 3                              #
+    #################################################################
+
     
     
 
